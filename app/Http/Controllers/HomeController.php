0a1ca6169f85e3c900fb5274766e1f5b60ab1b6b
abhi1221snva <42020778+abhi1221snva@users.noreply.github.com>
2024-09-09 14:10:20 +0530

    Ringless Cloud Site

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function aboutUs(){
+        return view('about-us');
+        
+    }
+
+ 
+
+    public function Solutions(){
+        return view('solutions');
+        
+    }
+
+    public function faq(){
+        return view('faq');
+        
+    }
+
+    public function contactUs(){
+        return view('contact-us');
+        
+    }
+
+
+
+
+
+   
+
+ 
+ 
+
+    
+
+
+    
+        }
+    
+
---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,179 @@
+@extends('web')
+@section('title', 'Best VOIP Phone Services  | Cloud Phone System |Cloud PBX Providers| USA, CANADA')
+
+@section('content')
+<body>
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+        <!-- Header Main Area -->
+
+        <!-- Header Main Area End Here -->
+
+        <!-- Title Bar -->
+        <div class="pbmit-title-bar-wrapper">
+            <div class="container">
+                <div class="pbmit-title-bar-content">
+                    <div class="pbmit-title-bar-content-inner">
+                        <div class="pbmit-tbar">
+                            <div class="pbmit-tbar-inner container">
+                                <h1 class="pbmit-tbar-title">Contact Us</h1>
+                            </div>
+                        </div>
+                        <div class="pbmit-breadcrumb">
+                            <div class="pbmit-breadcrumb-inner">
+                                <span><a title="" href="#" class="home"><span>Home</span></a></span>
+                                <span class="sep"><i class="pbmit-base-icon-angle-double-right"></i></span>
+                                <span><span class="post-root post post-post current-item">Contact Us</span></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Title Bar End-->
+
+        <!-- Contact Us Content -->
+        <div class="page-content">
+            <!-- Contact -->
+            <section class="contact-section-home1">
+                <div class="container">
+                    <div class="section-lg">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="pbmit-heading-subheading">
+                                    <h4 class="pbmit-subtitle-white">CONTACT WITH US</h4>
+                                    <h2 class="pbmit-title">
+                                        Feel free to get in touch with experts
+                                    </h2>
+                                    <div class="mt-2">
+                                        Interested in sending ringless voicemails? Send us a
+                                        message or schedule a demo with a Cafmotel team member by
+                                        submitting the form.
+                                    </div>
+                                </div>
+                                <ul class="pbmit-social-links">
+                                    <li class="pbmit-social-li pbmit-social-facebook">
+                                        <a href="#" target="_blank">
+                                            <span><i class="pbmit-base-icon-facebook-squared"></i></span>
+                                        </a>
+                                    </li>
+                                    <li class="pbmit-social-li pbmit-social-twitter">
+                                        <a href="#" target="_blank">
+                                            <span><i class="pbmit-base-icon-twitter"></i></span>
+                                        </a>
+                                    </li>
+                                    <li class="pbmit-social-li pbmit-social-instagram">
+                                        <a href="#" target="_blank">
+                                            <span><i class="pbmit-base-icon-instagram"></i></span>
+                                        </a>
+                                    </li>
+                                    <li class="pbmit-social-li pbmit-social-youtube">
+                                        <a href="#" target="_blank">
+                                            <span><i class="pbmit-base-icon-youtube-play"></i></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-7">
+                                <form class="contact-left-section contact-form" method="post" id="contact-form"
+                                    action="">
+                                    <div class="row g-4">
+                                        <div class="col-sm-12 col-md-6">
+                                            <input type="text" class="form-control mb-0" placeholder="Your Name"
+                                                name="Your Name" />
+                                        </div>
+                                        <div class="col-sm-12 col-md-6">
+                                            <input type="email" class="form-control" placeholder="Email Address"
+                                                name="Email Address" />
+                                        </div>
+                                        <div class="col-sm-12 col-md-6 mt-0">
+                                            <input type="text" class="form-control" placeholder="Phone Number"
+                                                name="Phone Number" />
+                                        </div>
+                                        <div class="col-sm-12 col-md-6 mt-0">
+                                            <input type="email" class="form-control" placeholder="subject"
+                                                name="subject" />
+                                        </div>
+                                        <div class="col-sm-12 mt-0">
+                                            <textarea class="form-control" placeholder="Write Message"
+                                                name="Write Message" data-gramm="false"
+                                                wt-ignore-input="true"></textarea>
+                                        </div>
+                                        <div class="mt-0">
+                                            <a href="#" class="pbmit-btn pbmit-btn-hover-secondary">Send a Massage</a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact-section-home1-bottom">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="pbmit-ihbox-style-20">
+                                    <div class="pbmit-ihbox-box d-flex align-items-center">
+                                        <div class="pbmit-ihbox-icon">
+                                            <div class="pbmit-ihbox-icon-wrapper">
+                                                <div class="pbmit-icon-wrapper pbmit-icon-type-icon">
+                                                    <i class="pbmit-xido-icon pbmit-xido-icon-location"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="pbmit-ihbox-contents">
+                                            <div class="pbmit-heading-desc">Visit Our Office</div>
+                                            <h2 class="pbmit-element-title">
+                                                169 Madison Ave STE 2945 New York, NY 10016
+                                            </h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="pbmit-ihbox-style-20">
+                                    <div class="pbmit-ihbox-box d-flex align-items-center">
+                                        <div class="pbmit-ihbox-icon">
+                                            <div class="pbmit-ihbox-icon-wrapper">
+                                                <div class="pbmit-icon-wrapper pbmit-icon-type-icon">
+                                                    <i class="pbmit-xido-icon-email"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="pbmit-ihbox-contents">
+                                            <div class="pbmit-heading-desc">Send Email</div>
+                                            <h2 class="pbmit-element-title">
+                                                <a href="mailto:sales@cafmotel.com">sales@cafmotel.com</a>
+                                            </h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="pbmit-ihbox-style-20">
+                                    <div class="pbmit-ihbox-box d-flex align-items-center">
+                                        <div class="pbmit-ihbox-icon">
+                                            <div class="pbmit-ihbox-icon-wrapper">
+                                                <div class="pbmit-icon-wrapper pbmit-icon-type-icon">
+                                                    <i class="pbmit-xido-icon pbmit-xido-icon-call"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="pbmit-ihbox-contents">
+                                            <div class="pbmit-heading-desc">Call Anytime</div>
+                                            <h2 class="pbmit-element-title">+1 844-614-2007</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                    src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=169 Madison Ave STE 2945 New York, NY 10016&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            </section>
+            <!-- Contact End -->
+        </div>
+        <!-- Contact Us Content End -->
+
+@endsection
